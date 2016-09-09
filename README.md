32420bebfd225c52b4f8a2c50006f99c6414ecd1
Disha Virk <disha@mylittlejob.de>
2016-09-09 07:52:08 +0200

    A GitHub Card App made with simple ReactJs

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a GitHub Card App made with simple ReactJs.
---
+++ script.jsx
@@ -0,0 +1,66 @@
+var Card = React.createClass({
+  getInitialState: function(){
+    return {};
+  },
+  componentDidMount: function(){
+    var component=this;
+    $.get("https://api.github.com/users/" + this.props.login, 
+    function(data){
+      component.setState(data);
+    });
+  },
+  render: function(){
+    return (
+      <div>
+      <img width="80" src={ this.state.avatar_url }></img>
+      <h3>{ this.state.name }</h3>
+      <hr/>
+      </div>
+    )
+  }
+});
+
+var Form = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+    var loginInput=React.findDOMNode(this.refs.login);
+    this.props.addCard(loginInput.value);
+    loginInput.value='';
+  },
+  render: function(){
+    return(
+      <form onSubmit={ this.handleSubmit }>
+        <input placeholder="Github Name" ref="login"/>
+        <button>Add</button>
+      </form>
+    );
+  }  
+});
+
+var Main = React.createClass({
+  getInitialState: function(){
+    return {
+      logins: []
+    }
+  },
+  addCard: function(loginToAdd){
+    this.setState({
+      logins: this.state.logins.concat(loginToAdd)
+    });
+  },
+  render: function(){
+      var cards = this.state.logins.map(function(login){
+       return (
+          <Card login={ login } />
+         )
+      });
+      return (
+        <div>
+          <Form addCard={ this.addCard }/>
+          {cards}
+        </div>
+    )
+  }
+});
+
+React.render(<Main />, document.getElementById("root"));
